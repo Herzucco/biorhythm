45d7c4ef27481feb68b2a51ebe06cb089754661f
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-05-24 22:04:44 +0200

    Input clavier/souris

--- Dev/Assets/Scripts/Greg/Bullet.cs
+++ Dev/Assets/Scripts/Greg/Bullet.cs
@@ -21,6 +21,9 @@ public class Bullet : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		if (other.gameObject.tag == "Player")
+		{
 			Destroy(this.gameObject);
+		}
 	}
 }
--- Dev/Assets/Scripts/Greg/ShipController.cs
+++ Dev/Assets/Scripts/Greg/ShipController.cs
@@ -6,6 +6,7 @@ public class ShipController : MonoBehaviour {
 	public float speed;
 	private float xMove;
 	private float yMove;
+	public string[] gamepadList;
 
 	// Use this for initialization
 	void Start () {
@@ -17,16 +18,30 @@ public class ShipController : MonoBehaviour {
 		xMove = Input.GetAxis("Horizontal") * speed;
 		yMove = Input.GetAxis("Vertical") * speed;
 
+		gamepadList = Input.GetJoystickNames();
+
 		this.rigidbody2D.transform.Translate(xMove, yMove, 0, Space.World);
 		if ((Mathf.Abs(Input.GetAxis("Vertical2")) >= 0.25) || (Mathf.Abs(Input.GetAxis("Horizontal2")) >= 0.25))
 		{
 			this.rigidbody2D.transform.localEulerAngles = new Vector3( 0, 0, Mathf.Atan2( Input.GetAxis("Vertical2"), Input.GetAxis("Horizontal2")) * Mathf.Rad2Deg - 90);
 			this.GetComponentInChildren<Gun>().enabled = true;
 		}
+		if (Input.GetButton("Shoot"))
+		{
+			this.GetComponentInChildren<Gun>().enabled = true;
+		}
 		else
 		{
 			this.GetComponentInChildren<Gun>().enabled = false;
 		}
-		//Debug.Log("X : " + xMove + " Y : " + yMove);
+
+		if (gamepadList.Length == 0)
+		{
+			Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			diff.Normalize();
+			float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+		}
+			//Debug.Log("X : " + xMove + " Y : " + yMove);
 	}
 }
