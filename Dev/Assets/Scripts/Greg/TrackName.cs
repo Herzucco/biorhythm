2e224cb4ab88131c98aa590d652342f2aa86ca4a
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-05-26 23:43:48 +0200

    TROP BIEN LE GAME LAAAAA

---
+++ Dev/Assets/Scripts/Greg/TrackName.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackName : MonoBehaviour {
+
+	private string audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().source.clip.name;
+		this.GetComponent<GUIText>().text = audioSource;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
