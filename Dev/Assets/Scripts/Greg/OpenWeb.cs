0769318832a9cce75b30c0288547ef103f79a6b2
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-05-27 03:18:44 +0200

    Les petites modifs

---
+++ Dev/Assets/Scripts/Greg/OpenWeb.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenWeb : MonoBehaviour {
+
+	public string urlAdress;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick() 
+	{
+		Application.OpenURL(urlAdress);
+	}
+}
